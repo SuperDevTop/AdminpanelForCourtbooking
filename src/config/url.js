eaeb86963985293076d59bca712b13f35b2f9de0
superdev <freelancerken2@gmail.com>
2023-12-05 19:46:31 -0800

    1261246

--- src/config/url.js
+++ src/config/url.js
@@ -2,10 +2,12 @@ let backendUrl;
 
 if (process.env.NODE_ENV === "development") {
   console.log("development env");
-  backendUrl = "http://localhost:5000";
+  // backendUrl = "http://localhost:5000";
+  backendUrl = "http://ec2-18-221-87-111.us-east-2.compute.amazonaws.com";
 } else {
   console.log("production env");
-  backendUrl = "https://courtbooking.vercel.app";
+  // backendUrl = "https://courtbooking.vercel.app";
+  backendUrl = "http://ec2-18-221-87-111.us-east-2.compute.amazonaws.com";
 }
 
 export { backendUrl };
